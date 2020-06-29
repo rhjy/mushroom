ba5595f535865701c828f4da80ba94f1cc6abff5
liang <595996918@qq.com>
2020-06-29 19:40:36 +0800

    完成首页静态页面

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,7 +5,38 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    duration: 500, // swiper滑动动画时长
+    interval: 5000, // 自动切换时间间隔
+    bannerImageList: [{
+        url: "http://192.168.14.80:3000/public/images/carousel/banner_01.png"
+      },
+      {
+        url: "http://192.168.14.80:3000/public/images/carousel/banner_02.png"
+      },
+      {
+        url: "http://192.168.14.80:3000/public/images/carousel/banner_03.png"
+      }
+    ],
+    courseList:[
+      {"icon":"http://192.168.14.80:3000/public/images/recommend/recommend1.png"},
+      {"icon":"http://192.168.14.80:3000/public/images/recommend/recommend2.png"},
+      {"icon":"http://192.168.14.80:3000/public/images/recommend/recommend3.png"},
+      {"icon":"http://192.168.14.80:3000/public/images/recommend/recommend4.png"},
+    ],
+    videoImageList:[
+      {"name":"可爱的玩偶制作教程","view_count":152,"cover_photo_url":"http://192.168.14.80:3000/public/images/hot_video/hot1.png","video_url":"http://vjs.zencdn.net/v/oceans.mp4"},
+      {"name":"编程几何的艺术魅力","view_count":623,"cover_photo_url":"http://192.168.14.80:3000/public/images/hot_video/hot2.png","video_url":"http://vjs.zencdn.net/v/oceans.mp4"},
+      {"name":"如何提升孩子空间想象力","view_count":451,"cover_photo_url":"http://192.168.14.80:3000/public/images/hot_video/hot3.png","video_url":"http://vjs.zencdn.net/v/oceans.mp4"},
+      {"name":"人工智能运算技术入门","view_count":232,"cover_photo_url":"http://192.168.14.80:3000/public/images/hot_video/hot4.png","video_url":"http://vjs.zencdn.net/v/oceans.mp4"},
+      {"name":"可爱的玩偶制作教程","view_count":152,"cover_photo_url":"http://192.168.14.80:3000/public/images/hot_video/hot1.png","video_url":"http://vjs.zencdn.net/v/oceans.mp4"},
+      {"name":"编程几何的艺术魅力","view_count":623,"cover_photo_url":"http://192.168.14.80:3000/public/images/hot_video/hot2.png","video_url":"http://vjs.zencdn.net/v/oceans.mp4"},
+      {"name":"如何提升孩子空间想象力","view_count":451,"cover_photo_url":"http://192.168.14.80:3000/public/images/hot_video/hot3.png","video_url":"http://vjs.zencdn.net/v/oceans.mp4"},
+      {"name":"人工智能运算技术入门","view_count":232,"cover_photo_url":"http://192.168.14.80:3000/public/images/hot_video/hot4.png","video_url":"http://vjs.zencdn.net/v/oceans.mp4"},
+      {"name":"可爱的玩偶制作教程","view_count":152,"cover_photo_url":"http://192.168.14.80:3000/public/images/hot_video/hot1.png","video_url":"http://vjs.zencdn.net/v/oceans.mp4"},
+      {"name":"编程几何的艺术魅力","view_count":623,"cover_photo_url":"http://192.168.14.80:3000/public/images/hot_video/hot2.png","video_url":"http://vjs.zencdn.net/v/oceans.mp4"},
+      {"name":"如何提升孩子空间想象力","view_count":451,"cover_photo_url":"http://192.168.14.80:3000/public/images/hot_video/hot3.png","video_url":"http://vjs.zencdn.net/v/oceans.mp4"},
+      {"name":"人工智能运算技术入门","view_count":232,"cover_photo_url":"http://192.168.14.80:3000/public/images/hot_video/hot4.png","video_url":"http://vjs.zencdn.net/v/oceans.mp4"},
+    ]
   },
 
   /**
